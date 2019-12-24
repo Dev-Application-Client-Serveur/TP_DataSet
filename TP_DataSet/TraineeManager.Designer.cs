976fd3ad3966a44209bfe8d113b8d471c5480d4e
essarraj.fouad <essarraj.fouad@gmail.com>
2019-12-24 17:39:07 +0100

    Ajoutez des fichiers projet.

---
+++ TP_DataSet/TraineeManager.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace TP_DataSet
+{
+    partial class TraineeManager
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.traineesDataGridView = new System.Windows.Forms.DataGridView();
+            this.traineesBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.traineesTableAdapter = new TP_DataSet.dbTableAdapters.TraineesTableAdapter();
+            this.db = new TP_DataSet.db();
+            this.idDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.firstNameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.lastNameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.birthdayDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.groupIdDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.traineesDataGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.traineesBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.db)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // traineesDataGridView
+            // 
+            this.traineesDataGridView.AutoGenerateColumns = false;
+            this.traineesDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.traineesDataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.idDataGridViewTextBoxColumn,
+            this.firstNameDataGridViewTextBoxColumn,
+            this.lastNameDataGridViewTextBoxColumn,
+            this.birthdayDataGridViewTextBoxColumn,
+            this.groupIdDataGridViewTextBoxColumn});
+            this.traineesDataGridView.DataSource = this.traineesBindingSource;
+            this.traineesDataGridView.Location = new System.Drawing.Point(12, 70);
+            this.traineesDataGridView.Name = "traineesDataGridView";
+            this.traineesDataGridView.Size = new System.Drawing.Size(547, 248);
+            this.traineesDataGridView.TabIndex = 1;
+            // 
+            // traineesBindingSource
+            // 
+            this.traineesBindingSource.DataMember = "Trainees";
+            this.traineesBindingSource.DataSource = this.db;
+            // 
+            // traineesTableAdapter
+            // 
+            this.traineesTableAdapter.ClearBeforeFill = true;
+            // 
+            // db
+            // 
+            this.db.DataSetName = "db";
+            this.db.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // idDataGridViewTextBoxColumn
+            // 
+            this.idDataGridViewTextBoxColumn.DataPropertyName = "Id";
+            this.idDataGridViewTextBoxColumn.HeaderText = "Id";
+            this.idDataGridViewTextBoxColumn.Name = "idDataGridViewTextBoxColumn";
+            // 
+            // firstNameDataGridViewTextBoxColumn
+            // 
+            this.firstNameDataGridViewTextBoxColumn.DataPropertyName = "FirstName";
+            this.firstNameDataGridViewTextBoxColumn.HeaderText = "FirstName";
+            this.firstNameDataGridViewTextBoxColumn.Name = "firstNameDataGridViewTextBoxColumn";
+            // 
+            // lastNameDataGridViewTextBoxColumn
+            // 
+            this.lastNameDataGridViewTextBoxColumn.DataPropertyName = "LastName";
+            this.lastNameDataGridViewTextBoxColumn.HeaderText = "LastName";
+            this.lastNameDataGridViewTextBoxColumn.Name = "lastNameDataGridViewTextBoxColumn";
+            // 
+            // birthdayDataGridViewTextBoxColumn
+            // 
+            this.birthdayDataGridViewTextBoxColumn.DataPropertyName = "Birthday";
+            this.birthdayDataGridViewTextBoxColumn.HeaderText = "Birthday";
+            this.birthdayDataGridViewTextBoxColumn.Name = "birthdayDataGridViewTextBoxColumn";
+            // 
+            // groupIdDataGridViewTextBoxColumn
+            // 
+            this.groupIdDataGridViewTextBoxColumn.DataPropertyName = "GroupId";
+            this.groupIdDataGridViewTextBoxColumn.HeaderText = "GroupId";
+            this.groupIdDataGridViewTextBoxColumn.Name = "groupIdDataGridViewTextBoxColumn";
+            // 
+            // TraineeManager
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(707, 363);
+            this.Controls.Add(this.traineesDataGridView);
+            this.Name = "TraineeManager";
+            this.Text = "TraineeManager";
+            this.Load += new System.EventHandler(this.TraineeManager_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.traineesDataGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.traineesBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.db)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.BindingSource traineesBindingSource;
+        private dbTableAdapters.TraineesTableAdapter traineesTableAdapter;
+        private System.Windows.Forms.DataGridView traineesDataGridView;
+        private System.Windows.Forms.DataGridViewTextBoxColumn idDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn firstNameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn lastNameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn birthdayDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn groupIdDataGridViewTextBoxColumn;
+        private db db;
+    }
+}
---
+++ TP_DataSet/TraineeManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TP_DataSet
+{
+    public partial class TraineeManager : Form
+    {
+        public TraineeManager()
+        {
+            InitializeComponent();
+        }
+
+        private void traineesBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.traineesBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.db);
+
+        }
+
+        private void TraineeManager_Load(object sender, EventArgs e)
+        {
+            // TODO: cette ligne de code charge les données dans la table 'db.Trainees'. Vous pouvez la déplacer ou la supprimer selon les besoins.
+            this.traineesTableAdapter.Fill(this.db.Trainees);
+
+        }
+    }
+}
